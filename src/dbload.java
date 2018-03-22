8d032d5b1b08c81d1e0d4fe96efaf6edf8b7cbd2
rmit-s3585502-chihyuan-chen <s3585502@student.rmit.edu.au>
2018-03-22 14:34:55 +1100

    Add files via upload
    Testing the function of reading csv file

--- src/dbload.java
+++ src/dbload.java
@@ -1,4 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class dbload {
-
+	BufferedReader input;
+	public void ReadFile() { //import txt file
+		try {
+			input = new BufferedReader(new FileReader("BUSINESS_NAMES_201803.csv"));
+		} catch (Exception e) {
+			System.out.println("Could not find info.txt");
+		}
+	}
 }
--- src/loadControl.java
+++ src/loadControl.java
@@ -1,4 +1,8 @@
 
 public class loadControl {
-
+	public static void main(String[] args) {
+		dbload db = new dbload();
+		db.ReadFile();
+		System.out.println("OK");
+	}
 }
