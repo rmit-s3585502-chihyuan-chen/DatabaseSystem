169fa393b578e8ec14bbc1e76030f788ab026336
rmit-s3585502-chihyuan-chen <s3585502@student.rmit.edu.au>
2018-03-22 17:18:44 +1100

    Add files via upload
    Load data from csv and output the data for testing the data has been loaded or has not been loaded.

--- src/dbload.java
+++ src/dbload.java
@@ -1,13 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class dbload {
 	BufferedReader input;
-	public void ReadFile() { //import txt file
+	public void ReadFile() {
 		try {
 			input = new BufferedReader(new FileReader("BUSINESS_NAMES_201803.csv"));
+			String[]output;
+			while(input.ready()){
+				output=input.readLine().split("/t");
+				for(int i=0;i<=output.length-1;i++){
+					System.out.println(output[i]);
+				}
+			    }
+				
+			
 		} catch (Exception e) {
-			System.out.println("Could not find info.txt");
+			System.err.println("Could not find info.txt");
 		}
 	}
+    
 }
