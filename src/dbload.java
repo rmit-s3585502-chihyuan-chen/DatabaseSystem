7054e5851d18ffcc449d4ae37c1b47a29ccb57cc
rmit-s3585502-chihyuan-chen <s3585502@student.rmit.edu.au>
2018-03-26 15:48:47 +1100

    Add files via upload
    Update to print out particular data from the csv file

--- src/dbload.java
+++ src/dbload.java
@@ -1,26 +1,45 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import org.apache.commons.io.*;
 public class dbload {
-	BufferedReader input;
+	String csvFile = "BUSINESS_NAMES_201803.csv";
+    BufferedReader br = null;
+    String line = "";
+    String cvsSplitBy = "\t";
+	
 	public void ReadFile() {
+		
 		try {
-			input = new BufferedReader(new FileReader("BUSINESS_NAMES_201803.csv"));
-			String[]output;
-			while(input.ready()){
-				output=input.readLine().split("/t");
-				for(int i=0;i<=output.length-1;i++){
-					System.out.println(output[i]);
-				}
-			    }
-				
-			
-		} catch (Exception e) {
-			System.err.println("Could not find info.txt");
-		}
-	}
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("NAME= " + country[1] + " , Status=" + country[2] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
     
 }
