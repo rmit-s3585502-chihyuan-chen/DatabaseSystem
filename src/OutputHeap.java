9332548d0a48bf6d7226bea1042ff7e825c99d70
rmit-s3585502-chihyuan-chen <s3585502@student.rmit.edu.au>
2018-03-28 01:59:47 +1100

    Add files via upload
    constructor of output heap file

---
+++ src/OutputHeap.java
@@ -0,0 +1,49 @@
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class OutputHeap {
+	DataOutputStream os;
+	public OutputHeap(String fileName) {
+	try{	
+			os= new DataOutputStream(new FileOutputStream("fileName"));
+	}catch (IOException e){
+       System.err.println("Cannot output file");
+	}	
+}
+public void	writeField(Field field){
+	try{	
+	if(field.getType().equals("String")){
+			writeBinary(field.getContent().getBytes("UTF-8"));
+		}
+	else{
+		os.writeLong(Long.valueOf(field.getContent()).longValue());
+	}
+	}catch (IOException e){
+	       System.err.println("Cannot write String into file");
+		}	
+}
+	public void writeInt(int index){
+		try{
+			os.writeInt(index);
+		}catch(IOException e){
+			 System.err.println("Cannot write Int into file");
+		}
+		
+	}
+	public void writeShort(Short shortValue ){
+		try{
+			os.writeShort(shortValue);
+		}catch(IOException e){
+			System.err.println("Cannot write Short into file");
+		}
+	}
+	public void writeBinary(byte []bytes ){
+		try{
+			os.write(bytes);
+		}catch(IOException e){
+			System.err.println("Cannot write Short into file");
+		}
+	}
+}
