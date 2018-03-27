5320666c53af211f1320ee541c1e1ef9b04afcc1
rmit-s3585502-chihyuan-chen <s3585502@student.rmit.edu.au>
2018-03-28 01:59:05 +1100

    Add files via upload
    update to import all data into heap file and output it

--- src/dbload.java
+++ src/dbload.java
@@ -4,23 +4,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
+
+
+
+
+
 public class dbload {
 	
 	public static void main(String[] args) throws IOException {
 	int pagesize=4096;
 	String csvFile = "BUSINESS_NAMES_201803.csv";
     String cvsSplitBy = "\t";
-    ArrayList<filed> fieldList = new ArrayList<>();
-    ArrayList<record> recordList = new ArrayList<>();
-    ArrayList<page> pagelist = new ArrayList<>();
+    ArrayList<Field> fieldList = new ArrayList<>();
+    ArrayList<Record> recordList = new ArrayList<>();
+    ArrayList<Page> pagelist = new ArrayList<>();
 	int filedNumber=0;
 	int length;
+	int index ;
 	String type;
 	String content; 
 	BufferedReader br = null;
      String line = "";
 		try {
-			page page = new page();
+			Page page = new Page();
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
             		fieldList =  new ArrayList<>();
@@ -36,25 +43,47 @@ public class dbload {
             			}
             		}
             		
-            		fieldList.add(new filed("String",elements[0]));
-            		fieldList.add(new filed("String",elements[1]));
-            		fieldList.add(new filed("String",elements[2]));
-            		fieldList.add(new filed("String",elements[3]));
-            		fieldList.add(new filed("String",elements[4]));
-            		fieldList.add(new filed("String",elements[5]));
-            		fieldList.add(new filed("String",elements[6]));
-            		fieldList.add(new filed("String",elements[7]));
-            		fieldList.add(new filed("long",elements[8]));
-            		record record = new record(fieldList);
+            		fieldList.add(new Field("String",elements[0]));
+            		fieldList.add(new Field("String",elements[1]));
+            		fieldList.add(new Field("String",elements[2]));
+            		fieldList.add(new Field("String",elements[3]));
+            		fieldList.add(new Field("String",elements[4]));
+            		fieldList.add(new Field("String",elements[5]));
+            		fieldList.add(new Field("String",elements[6]));
+            		fieldList.add(new Field("String",elements[7]));
+            		fieldList.add(new Field("long",elements[8]));
+            		Record record = new Record(fieldList);
             		if(pagesize - page.getLength()>record.getLength()){
             			page.add(record);
             	}       
             	else{
             			pagelist.add(page);
-            			page = new page();
+            			page = new Page();
             			page.add(record);
             	}
             		
+            		
+            		//for(Page p:pagelist){
+            		//	for(Record r:p.getRecord()){
+            		//		for(Field f:r.getField()){
+            		//			System.out.println(f.getContent());
+            				
+            		//			}
+            		//		}
+            		//	}
+            		}
+            		
+            OutputHeap out=new OutputHeap("heap."+pagesize);
+            for(int i=0;i<pagelist.size();i++){
+            out.writeInt(page.getRecord().size());
+            index=2+(pagelist.size()*2);
+            System.out.print(index);
+            for(int j=0;j<pagelist.get(i).getRecord().size();j++){
+            	out.writeInt(index);
+            	index+=pagelist.get(i).getRecord().get(j).getLength();
+            }
+            
+            	
             }
 		
 		
